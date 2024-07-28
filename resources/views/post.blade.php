852a7587e2a7fe8cbad56d5f4dc0a913811e9a13
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-07-28 14:43:30 +0800

    added comment function

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,22 +5,40 @@
     <div class="space-y-5">
         <h1 class="font-bold text-gray-900 text-lg">{{ ucfirst($user->username) }} Posts</h1>
         <div class="grid grid-cols-2 gap-4">
-            @foreach($user->posts as $post)
-                <section class="hover:bg-white border border-gray-300 rounded-lg p-5 h-auto drop-shadow-lg">
-                    <h1 class="font-bold text-gray-900 text-xs">{{ $post->title }}</h1>
-                    <p class="text-black text-md">{{ $post->body }}</p>
-                    <div class="border-b border-gray-300 w-full my-5"></div>
-                    <section class="block space-y-2">
-                        <h1 class="font-bold text-gray-900 text-lg">Comments</h1>
-                        @foreach($post->comments as $comment)
-                            <section class="flex flex-col border-b border-gray-300 p-5">
-                                <a href="/post/{{ $comment->user->id }}" class="underline font-bold text-blue-900 text-xs">{{ $comment->user->username }}</a>
-                                <p class="text-black text-md">{{ $comment['body'] }}</p>
-                            </section>
-                        @endforeach
+            @if(count($user->posts) > 0)
+                @foreach($user->posts as $post)
+                    <section class="hover:bg-white border border-gray-300 rounded-lg p-5 h-auto drop-shadow-lg">
+                        <h1 class="font-bold text-gray-900 text-xs">{{ $post->title }}</h1>
+                        <p class="text-black text-md">{{ $post->body }}</p>
+                        <div class="border-b border-gray-300 w-full my-5"></div>
+                        <section class="block space-y-2">
+                            <h1 class="font-bold text-gray-900 text-lg">Comments</h1>
+                            @foreach($post->comments as $comment)
+                                <section class="flex flex-col border-b border-gray-300 p-5">
+                                    <a href="/post/{{ $comment->user->id }}" class="underline font-bold text-blue-900 text-xs">{{ $comment->user->username }}</a>
+                                    <p class="text-black text-md">{{ $comment['body'] }}</p>
+                                </section>
+                            @endforeach
+                        </section>
+                        <form action="/post/{{ $post->id }}/comment/{{ $user->id }}" method="POST" class="rounded-lg p-5 w-full space-y-3">
+                            @csrf
+                            <input type="text"
+                                   name="body"
+                                   placeholder="add comment..."
+                                   class="h-10 rounded-md border border-gray-300 w-full"
+                            >
+                            @error('body')
+                                <p class="text-xs text-red-500 font-semibold">{{ $message }}</p>
+                            @enderror
+                            <div class="flex justify-end">
+                                <button class="py-1 px-5 rounded-lg bg-blue-500 text-white text-xs">Add</button>
+                            </div>
+                        </form>
                     </section>
-                </section>
-            @endforeach
+                @endforeach
+            @else
+                <h1 class="font-bold text-gray-900 text-lg">NO POST TO SHOW</h1>
+            @endif
         </div>
         <a href="/" class="underline">Back to home</a>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +14,22 @@ Route::get('/', function () {
 Route::get('/post/{id}', function ($id) {
     $users = User::with('posts', 'comments')->find($id);
     return view('post', ["heading" => "Create a post", 'user' => $users]);
+});
+
+
+// store
+Route::post('/post/{postId}/comment/{userId}', function ($postId, $userId) {
+    // Validate input
+    request()->validate([
+        'body' => 'required'
+    ]);
+    // create new comment associated with the post id
+    Comment::create([
+        'user_id' => '6',
+        'post_id' => $postId,
+        'body' => request('body')
+    ]);
+    return redirect('/post/' . $userId);
 });
 
 
